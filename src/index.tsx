3869ff99c8a1ca434652516e3da10441db619bc3
Prabeshpd <39412015+Prabeshpd@users.noreply.github.com>
2022-10-31 06:30:05 +0545

    feat: add webpack configuration

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+// import { Provider } from 'react-redux';
+// import { IntlProvider } from 'react-intl';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container!);
+
+root.render(<div id="app">asf</div>);

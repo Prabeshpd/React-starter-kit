1e39cbceb22748d52a181363a1ae4bb7b1eaf203
Prabeshpd <39412015+Prabeshpd@users.noreply.github.com>
2022-11-03 15:17:45 +0545

    feat: add store and base component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,52 @@
 import * as React from 'react';
-// import { Provider } from 'react-redux';
-// import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import * as interceptors from './utils/interceptors';
+
+import 'font-awesome/css/font-awesome.css';
+import 'foundation-sites/dist/css/foundation.min.css';
+
+import store, { persistor } from './store';
 import { createRoot } from 'react-dom/client';
+
+import Routes from './components/router';
+import http from './utils/http';
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
+function initInterceptors() {
+  http.interceptors.response.use(
+    /**
+     * Leave response as it is.
+     *
+     * @param {any} response
+     */
+    (response) => response,
+    /**
+     * This interceptor checks if the response had a 401 status code, which means
+     * that the access token used for the request has expired. It then refreshes
+     * the access token and resends the original request.
+     */
+    interceptors.unauthorizedResponseHandlerInterceptor
+  );
+  http.interceptors.request.use(interceptors.authorizationInterceptor);
+  http.interceptors.request.use(
+    /**
+     * Leave Error as it is.
+     *
+     * @param {any} err
+     */
+    (err) => err
+  );
+}
+initInterceptors();
 
-root.render(<div id="app">asf</div>);
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <div id="app">
+        <Routes />
+      </div>
+    </PersistGate>
+  </Provider>
+);

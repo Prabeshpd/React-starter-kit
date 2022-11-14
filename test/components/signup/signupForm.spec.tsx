d2964c8b9bed578445b1e1e68a94a23a3d69e76e
Prabeshpd <prabesh1995nepalktm@gmail.com>
2022-11-14 21:02:43 +0545

    refactor: test suite of signup form

--- test/components/signup/signupForm.spec.tsx
+++ test/components/signup/signupForm.spec.tsx
@@ -1,51 +1,52 @@
 import * as React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { render, waitFor, RenderResult } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { SignUpForm } from '../../../src/components/signup/signupForm';
 
-test('SignupForm to be rendered with needed element', () => {
+describe('singup Form:', () => {
   const handleFormSubmit = jest.fn();
+  let signUpComponent: RenderResult;
 
-  let signUpComponent = render(<SignUpForm handleFormSubmit={handleFormSubmit} />);
+  beforeEach(() => {
+    signUpComponent = render(<SignUpForm handleFormSubmit={handleFormSubmit} />);
+  });
 
-  const inputNameElement = signUpComponent.getByPlaceholderText('Name');
-  expect(inputNameElement).toHaveAttribute('type', 'text');
-  expect(inputNameElement).toHaveValue('');
+  test('SignupForm to be rendered with needed element', () => {
+    const inputNameElement = signUpComponent.getByPlaceholderText('Name');
+    expect(inputNameElement).toHaveAttribute('type', 'text');
+    expect(inputNameElement).toHaveValue('');
 
-  const inputEmailElement = signUpComponent.getByPlaceholderText('email');
-  expect(inputEmailElement).toHaveAttribute('type', 'email');
-  expect(inputEmailElement).toHaveValue('');
+    const inputEmailElement = signUpComponent.getByPlaceholderText('email');
+    expect(inputEmailElement).toHaveAttribute('type', 'email');
+    expect(inputEmailElement).toHaveValue('');
 
-  const inputPasswordElement = signUpComponent.getByPlaceholderText('password');
-  expect(inputPasswordElement).toHaveAttribute('type', 'password');
-  expect(inputPasswordElement).toHaveValue('');
+    const inputPasswordElement = signUpComponent.getByPlaceholderText('password');
+    expect(inputPasswordElement).toHaveAttribute('type', 'password');
+    expect(inputPasswordElement).toHaveValue('');
 
-  const submitButtonElement = signUpComponent.getByRole('button', { name: 'Save' });
-  expect(submitButtonElement).toHaveAttribute('type', 'submit');
+    const submitButtonElement = signUpComponent.getByRole('button', { name: 'Save' });
+    expect(submitButtonElement).toHaveAttribute('type', 'submit');
 
-  const resetButtonElement = signUpComponent.getByRole('button', { name: 'Cancel' });
-  expect(resetButtonElement).toHaveAttribute('type', 'reset');
-});
+    const resetButtonElement = signUpComponent.getByRole('button', { name: 'Cancel' });
+    expect(resetButtonElement).toHaveAttribute('type', 'reset');
+  });
 
-test('SignupForm submits value of form on submit', async () => {
-  const handleFormSubmit = jest.fn();
+  test('SignupForm submits value of form on submit', async () => {
+    const user = userEvent.setup();
 
-  let signUpComponent = render(<SignUpForm handleFormSubmit={handleFormSubmit}></SignUpForm>);
+    await user.type(signUpComponent.getByPlaceholderText('Name'), 'John');
+    await user.type(signUpComponent.getByPlaceholderText('email'), 'john.dee@gmail.com');
+    await user.type(signUpComponent.getByPlaceholderText('password'), 'Admin@123');
 
-  const user = userEvent.setup();
+    await user.click(signUpComponent.getByRole('button', { name: /save/i }));
 
-  await user.type(signUpComponent.getByPlaceholderText('Name'), 'John');
-  await user.type(signUpComponent.getByPlaceholderText('email'), 'john.dee@gmail.com');
-  await user.type(signUpComponent.getByPlaceholderText('password'), 'Admin@123');
-
-  await user.click(signUpComponent.getByRole('button', { name: /save/i }));
-
-  await waitFor(() =>
-    expect(handleFormSubmit).toHaveBeenCalledWith({
-      email: 'john.dee@gmail.com',
-      name: 'John',
-      password: 'Admin@123',
-    })
-  );
+    await waitFor(() =>
+      expect(handleFormSubmit).toHaveBeenCalledWith({
+        email: 'john.dee@gmail.com',
+        name: 'John',
+        password: 'Admin@123',
+      })
+    );
+  });
 });
